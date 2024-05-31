00daaae4aae81f32b71a478b492250e0c88a3805
Salah Louktaila <86385687+AmirLouktaila@users.noreply.github.com>
2024-05-31 11:19:02 +0300

    se index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ async function downloadImage(url) {
     return Buffer.from(response.data, 'binary');
 }
 
-async function blurImage(buffer, blurAmount = 20) {
+async function blurImage(buffer, blurAmount = 12) {
     return await sharp(buffer)
         .blur(blurAmount)
         .toBuffer();
